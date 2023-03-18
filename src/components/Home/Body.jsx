5c9bf71671ce97e1fa4182a8cc457623421b5407
Adeyemi-Femi <bossfemzy@gmail.com>
2023-03-18 03:26:47 +0100

    added an update

--- src/components/Home/Body.jsx
+++ src/components/Home/Body.jsx
@@ -40,14 +40,14 @@ const Body = ({ darkMode}) => {
                     </a>
                 </div>
             </div>
-            <div className=' w-full md:w-[50%] relative h-[50vh] md:my-0 my-[11%]'>
-                <div className="border-2  shadow-indigo-500 shadow-sm w-[350px] h-[350px]
-                     border-indigo-200 rounded-full absolute md:left-0 left-[1%]
-                     md:-top-[5%]  top-[10%] md:right-[15%] right-[4%] md:h-[400px] md:w-[400px]"></div>
+            <div className=' w-full md:w-[50%] relative h-[50vh] md:my-0 my-[11%] '>
+                <div className="border-2  shadow-indigo-500 shadow-sm w-[330px] h-[330px]
+                     border-indigo-200 rounded-full absolute md:left-0 md:ml-2
+                     md:-top-[5%]  top-[10%] md:right-[10%] right-[4%] md:h-[400px] md:w-[400px]"></div>
                 <img src="/images/pic.jpg" alt="" className='grayscale 
-                    h-100 w-[40%] rounded-full absolute md:-top-[10%] top-[5%] left-[5%]'/>
+                    h-100 md:w-[30%] w-[40%] rounded-full absolute md:-top-[10%] top-[3%] left-[5%]'/>
                 <div className=" bg-gradient-to-r from-[#9060DA] to-[rgb(40,162,207)] w-[130px] h-[130px]
-                    rounded-full absolute md:-bottom-[15%] -bottom-[7%] right-[10%] md:h-[150px] md:w-[150px]"></div>
+                    rounded-full absolute md:-bottom-[15%] -bottom-[7%] md:right-[30%] right-[15%] md:h-[150px] md:w-[150px]"></div>
             </div>
             <div className="md:flex hidden flex-col justify-evenly mr-[2%]">
             <a href="https://web.facebook.com/profile.php?id=100007274899219" target="_blank" rel="noopener noreferrer">
